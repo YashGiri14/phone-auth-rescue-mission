243aba128829ba87a5729ca36082ae9f73cb1832
Yash Giri <yashgiri1414@gmail.com>
2025-06-16 20:16:14 +0530

    changes to firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -18,20 +18,20 @@
 
 
 
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBgnyhv_YOlsZs-XKrMiIY_H-BaIHRFfAA",
-  authDomain: 'artizanmobileverify.firebaseapp.com',
-  projectId: 'artizanmobileverify',
-  storageBucket: 'artizanmobileverify.firebasestorage.app',
-  messagingSenderId: '889287217106',
-  appId: '1:889287217106:web:27ae9397584b454b71020b'
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBgnyhv_YOlsZs-XKrMiIY_H-BaIHRFfAA",
+//   authDomain: 'artizanmobileverify.firebaseapp.com',
+//   projectId: 'artizanmobileverify',
+//   storageBucket: 'artizanmobileverify.firebasestorage.app',
+//   messagingSenderId: '889287217106',
+//   appId: '1:889287217106:web:27ae9397584b454b71020b'
+// };
 
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase app
+// const app = initializeApp(firebaseConfig);
 
-// Export Auth instance
-export const auth = getAuth(app);
+// // Export Auth instance
+// export const auth = getAuth(app);
